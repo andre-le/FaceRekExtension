4c2f716d797e22c56d7ef41f202ab985d9958abe
tienanh2007 <nguyentienanh68@gmail.com>
2019-03-03 05:44:45 -0500

    password

--- extension/popup.js
+++ extension/popup.js
@@ -5,6 +5,18 @@ const context = canvas.getContext("2d")
 if (localStorage.getItem("facerek")) $("#sites").show()
 else $("#landing").show()
 
+hashCode = function(string) {
+  var hash = 0, i, chr;
+  if (string.length === 0) return hash;
+  for (i = 0; i < string.length; i++) {
+    chr   = string.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.srcObject = stream
@@ -19,17 +31,32 @@ $("#newProfile").click(function() {
 })
 
 $("#snap").click(function() {
-    context.drawImage(video, 0, 0, 400, 300)
+    context.drawImage(video, 0, 0, 200, 200)
     $("#videoBlock").hide()
     $("#photoBlock").show()
 })
 
 $("#save").click(function() {
     localStorage.setItem("facerek", canvas.toDataURL())
-    $("#face").fadeOut(function() {
-        $("#sites").fadeIn()
-    })
+    // $("#face").fadeOut(function() {
+    //     $("#sites").fadeIn()
+    // })
+    $("#passwordBlock").show();
+    $("#photoBlock").hide()
 })
+
+$("#savePassword").click(function() {
+    match = $("#pass1").val() != $("#pass2").val();
+    if($("#pass1").val().length == 0) return;
+    if(match) $("#error").show();
+    else {
+      hash = hashCode($("#pass1").val());
+      localStorage.setItem("password", hash);
+      $("#face").fadeOut(function() {
+          $("#sites").fadeIn();
+      })
+    }
+});
 
 $("#retake").click(function() {
     $("#photoBlock").hide()
@@ -43,15 +70,5 @@ $("#add").click(function() {
 
 // Trigger photo take
 document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
+	context.drawImage(video, 0, 0, 200, 200);
 });
-
-next = document.getElementById('next');
-next.onclick = () => {
-  chrome.browserAction.setPopup({popup: "login.html"});
-}
-
-back = document.getElementById('back');
-back.onclick = () => {
-  chrome.browserAction.setPopup({popup: "popup.html"});
-}
